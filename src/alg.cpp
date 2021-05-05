d33acb4d0d746fb742ae927cc6d7eebb15eb11b5
Sergey1339 <79190170+Sergey1339@users.noreply.github.com>
2021-05-05 22:16:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,46 +1,40 @@
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
 #include <cassert>
- 
 template<typename T>
-class TQueue
-{
-private:
-    T *arr;
-    int size;
-    int begin,
-        end;
-    int count;
-public:
-    TQueue(int =100);
-    ~TQueue();
- 
-    void push(const T &);
-    T pop();
-    T get() const;
-    bool isEmpty() const;
-    bool isFull() const;
-};
+class TPQueue {
+ private:
+  T *arr;
+  int size;
+  int begin, end;
+  int count;
+  int stepBack(int index) {
+    int res = --index;
+    if (res < 0)
+        res += size + 1;
+    return res;
+  }
+  int stepForward(int index) {
+    int res = ++index;
+    if (res > size)
+        res -= size + 1;
+    return res;
+  }
 
-template<typename T>
-TQueue<T>::TQueue(int sizeQueue) :
-    size(sizeQueue), 
-    begin(0), end(0), count(0)
-{
+ public:
+  TPQueue() :
+    size(100),
+    begin(0), end(0), count(0) {
     arr = new T[size + 1];
-}
-
-template<typename T>
-TQueue<T>::~TQueue()
-{
-    delete [] arr;
-}
-
-
-
-template<typename T>
-void TQueue<T>::push(const T & item)
-{
-    assert( count < size );
-  if (count != 0 && item.prior > arr[count - 1].prior){
+  }
+  ~TPQueue() {
+    delete[] arr;
+  }
+	
+  void push(const T &item) {
+    assert(count < size);
+    if (count != 0 && item.prior > arr[count - 1].prior){
     arr[end++] = item;
     count++;
     for (int i = 1; i< count; i++)
@@ -54,44 +48,29 @@ void TQueue<T>::push(const T & item)
     count++;
   }
     if (end > size)
-        end -= size + 1; 
-}
-
-template<typename T>
-T TQueue<T>::pop()
-{
-    assert( count > 0 );
- 
-    T item = arr[begin++];
+        end -= size + 1;
+  }
+	
+  T pop() {
+    assert(count > 0);
+    T item = arr[begin];
     count--;
- 
-    if (begin > size)
-        begin -= size + 1;
- 
+    begin = stepForward(begin);
     return item;
-}
-
-template<typename T>
-T TQueue<T>::get() const 
-{
-    assert( count > 0 );
+  }
+  T get() const {
+    assert(count > 0);
     return arr[begin];
-}
-
-template<typename T>
-bool TQueue<T>::isEmpty() const
-{
-  return count==0;
-}
-
-template<typename T>
-bool TQueue<T>::isFull() const
-{
-  return count==size;
-}
-
-struct SYM
-{
-	char ch;
-	int prior;
-}
+  }
+  bool isEmpty() const {
+    return count == 0;
+  }
+  bool isFull() const {
+    return count == size;
+  }
+};
+struct SYM {
+  char ch;
+  int  prior;
+};
+#endif // INCLUDE_TPQUEUE_H_
